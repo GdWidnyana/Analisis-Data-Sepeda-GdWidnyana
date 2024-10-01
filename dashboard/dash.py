03abe2ed0f7ab3824b8e9b967b99260a231efabe
GdWidnyana <120539660+GdWidnyana@users.noreply.github.com>
2024-10-01 19:58:55 +0800

    Update dash.py

--- dashboard/dash.py
+++ dashboard/dash.py
@@ -273,45 +273,31 @@ all_df['hour'] = all_df['date'].dt.hour
 
 # Now you can safely group by 'hour' for visualization
 hourly_counts = all_df.groupby('hour')['count'].mean().reset_index()
-
-# # Plotting the data
-# fig = px.line(
-#     hourly_counts,
-#     x='hour',
-#     y='count',
-#     title='Average Number of Bike Rentals per Hour',
-#     labels={'hour': 'Hour of Day', 'count': 'Average Rentals'}
-# )
-
-# st.plotly_chart(fig)
-
-# # # Visualisasi 3: Jumlah Penyewaan Sepeda per Jam
-# # st.subheader('Tren Jumlah Penyewaan Sepeda per Jam')
-# # hourly_counts = all_df.groupby('hour')['count'].mean().reset_index()
-# # fig = px.line(
-# #     hourly_counts,
-# #     x='hour',
-# #     y='count',
-# #     markers=True,
-# #     labels={'hour': 'hour', 'count': 'jumlah sewa'},
-# #     title="perkembangan jumlah sewa setiap jam ",
-# #     template='plotly'
-# # )
-# # st.plotly_chart(fig)
-
-# Visualisasi 3: Jumlah Penyewaan Sepeda per Jam
-st.subheader('Tren Jumlah Penyewaan Sepeda per Jam')
-hourly_counts = all_df.groupby('hour')['count'].mean().reset_index()
+# st.subheader('Tren Jumlah Penyewaan Sepeda per Jam')
+# Plotting the data
 fig = px.line(
     hourly_counts,
     x='hour',
     y='count',
-    markers=True,
-    labels={'hour': 'jam', 'count': 'jumlah sewa'},
-    title="perkembangan jumlah sewa setiap jam ",
-    template='plotly'
+    title='Average Number of Bike Rentals per Hour',
+    labels={'hour': 'Hour of Day', 'count': 'Average Rentals'}
 )
+
 st.plotly_chart(fig)
+
+# # Visualisasi 3: Jumlah Penyewaan Sepeda per Jam
+# st.subheader('Tren Jumlah Penyewaan Sepeda per Jam')
+# hourly_counts = all_df.groupby('hour')['count'].mean().reset_index()
+# fig = px.line(
+#     hourly_counts,
+#     x='hour',
+#     y='count',
+#     markers=True,
+#     labels={'hour': 'jam', 'count': 'jumlah sewa'},
+#     title="perkembangan jumlah sewa setiap jam ",
+#     template='plotly'
+# )
+# st.plotly_chart(fig)
 
 
 st.write("------------------------------------------------")
